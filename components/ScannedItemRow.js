5e4e0728340e4b4b6ac01ffa0327c7dcfc678fc3
Jens Plüddemann <jens.plueddemann@transmit.de>
2019-07-08 16:06:35 +0200

    added ScannedItemRow and ScannedItemList

---
+++ components/ScannedItemRow.js
@@ -0,0 +1,52 @@
+let React = require('react');
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 10,
+        marginLeft:16,
+        marginRight:16,
+        marginTop: 8,
+        marginBottom: 8,
+        borderRadius: 5,
+        backgroundColor: '#FFF',
+        elevation: 2,
+    },
+    title: {
+        fontSize: 16,
+        color: '#000',
+    },
+    container_text: {
+        flex: 1,
+        flexDirection: 'column',
+        marginLeft: 12,
+        justifyContent: 'center',
+    },
+    description: {
+        fontSize: 11,
+        fontStyle: 'italic',
+    },
+    photo: {
+        height: 50,
+        width: 50,
+    },
+});
+
+const ScannedItemRow = ({ name, /* image  */}) => {
+    console.log(name);
+    return (
+        <View style={styles.container}>
+            {/* <Image source={{ uri: image }} style={styles.photo} /> */}
+            <View style={styles.container_text}>
+                <Text style={styles.title}>
+                    {name}
+                </Text>
+            </View>
+
+        </View>
+    )
+};
+
+export default ScannedItemRow;
---
+++ components/ScannedItemsList.js
@@ -0,0 +1,27 @@
+let React = require('react');
+import { View, FlatList, StyleSheet, Text } from 'react-native';
+import ScannedItemRow from './ScannedItemRow';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
+
+
+const ScannedItemView = ({ itemList }) => {
+    console.log(itemList);
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={itemList}
+                renderItem={({ item }) =><ScannedItemRow
+                    name={item.name}
+                    /* image={item.image} */
+                />}
+            />
+        </View>
+    );
+};
+
+export default ScannedItemView;
