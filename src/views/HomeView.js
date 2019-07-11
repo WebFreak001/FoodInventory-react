66de3f93fa8bac773f4a7ed8845b5c44bb46dd5c
WebFreak001 <gh@webfreak.org>
2019-07-11 20:09:16 +0200

    move sources into src folder

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -5,6 +5,9 @@ import ScannedItemList from '../components/ScannedItemsList';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+// @ts-ignore
+const fridgeServer = require("../config.json");
+
 // https://material.io/tools/color/#!/?view.left=1&amp;view.right=0&view.right=0&primary.color=651FFF
 
  const styles = StyleSheet.create({
@@ -29,7 +32,6 @@ export default class HomeView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fridgeServer: "http://127.0.0.1:3000",
       fridgeID: "5d1d9c85af6f44db0ac6dac0",
       api: {
         "label": "Fridge loading",
@@ -44,10 +46,14 @@ export default class HomeView extends Component {
   };
 
   async loadAPI() {
-    var data = await fetch(this.state.fridgeServer + "/api/fridge/" + this.state.fridgeID);
+    var data = await fetch(config.serverURL + "/api/fridge/" + this.state.fridgeID, {
+      method: "GET",
+      headers: {
+        "User-Agent": "FoodInventory-react v0.1.0"
+      }
+    });
     var ret = await data.json();
     this.setState({
-      fridgeServer: "http://127.0.0.1:3000",
       fridgeID: "5d1d9c85af6f44db0ac6dac0",
       api: ret
     });
